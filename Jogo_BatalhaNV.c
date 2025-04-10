10f65f2db9a3cac25ca2469787daff8249eca288
Jonathan-dv-ia <202501286399@alunos.estacio.br>
2025-04-10 14:21:44 -0300

    Update Jogo_BatalhaNV.c

--- Jogo_BatalhaNV.c
+++ Jogo_BatalhaNV.c
@@ -16,6 +16,12 @@ int main() {
     for (int i = 0; i < NAVIO; i++) { // Loop para posicionar o navio horizontalmente
         tabuleiro[2][3 + i] = 3; // Marca as posições do navio horizontalmente com 3
         tabuleiro[5 + i][1] = 3; // Marca as posições do navio verticalmente com 3
+         tabuleiro [1][9] = 3; // Marca  uma posição diagonal - direita para esquerda
+        tabuleiro [2][8] = 3; // Marca  uma posição diagonal - direita para esquerda
+        tabuleiro [3][7] = 3; // Marca  uma posição diagonal - direita para esquerda
+        tabuleiro [1][1] = 3; // Marca  uma posição vertical - esquerda para direita
+        tabuleiro [2][2] = 3; // Marca  uma posição vertical - esquerda para direita
+        tabuleiro [3][3] = 3; // Marca  uma posição vertical - esquerda para direita
     }
 
     // Exibe o tabuleiro
@@ -23,15 +29,15 @@ int main() {
     for (int i = 0; i < TAMANHO; i++) { // Loop para imprimir as letras das colunas
         printf("%c ", linhas[i]); // Imprime cada letra da coluna seguida de um espaço
     }
-    printf("\n"); // Imprime uma nova linha
+    printf("\n"); 
 
     for (int i = 0; i < TAMANHO; i++) { // Loop para percorrer as linhas do tabuleiro
         printf("%2d ", i); // Imprime o número da linha (com 2 espaços para alinhamento)
         for (int j = 0; j < TAMANHO; j++) { // Loop para percorrer as colunas do tabuleiro
             printf("%d ", tabuleiro[i][j]); // Imprime o valor da posição do tabuleiro seguido de um espaço
         }
-        printf("\n"); // Imprime uma nova linha após cada linha do tabuleiro
+        printf("\n"); 
     }
 
-    return 0; // Indica que o programa terminou com sucesso
+    return 0; 
 }
